14ee35329e3cfd3133be2831225f75fc57bf41ab
juangimenezmh <juan.gimenez@mightyhive.com>
2022-11-05 19:30:28 -0300

    reverting merge

--- app/src/main/java/com/example/gestionpresupuesto/fragments/menu/containerFragmentBudget/BudgetWithItemsDetailsFragment.kt
+++ app/src/main/java/com/example/gestionpresupuesto/fragments/menu/containerFragmentBudget/BudgetWithItemsDetailsFragment.kt
@@ -23,7 +23,6 @@ class BudgetWithItemsDetailsFragment : Fragment() {
     private lateinit var budgetDate: TextView
     private lateinit var budgetExpirationDate: TextView
     private lateinit var clientName: TextView
-    private lateinit var clientCuit_dni: TextView
     private lateinit var clientDomicile: TextView
     private lateinit var clientBetweenStreet1: TextView
     private lateinit var clientBetweenStreet2: TextView
@@ -71,9 +70,6 @@ class BudgetWithItemsDetailsFragment : Fragment() {
 
         clientName = v.findViewById(R.id.budget_clientName)
         clientName.text = budgetDetails.clientName
-
-        clientCuit_dni = v.findViewById(R.id.budget_cuit_dni)
-        clientCuit_dni.text = budgetDetails.clientDNI_CUIT.toString()
 
         clientDomicile = v.findViewById(R.id.budget_clientDomicile)
         clientDomicile.text = budgetDetails.clientDomicile
