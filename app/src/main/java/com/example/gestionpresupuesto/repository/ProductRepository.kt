5e05676efaf79edfae4f6f2f8a24429101d16f8d
Fernando Bernasconi <fernando.bernasconi@globant.com>
2022-11-12 22:19:06 -0300

    added snack bar when delete product

--- app/src/main/java/com/example/gestionpresupuesto/repository/ProductRepository.kt
+++ app/src/main/java/com/example/gestionpresupuesto/repository/ProductRepository.kt
@@ -2,6 +2,7 @@ package com.example.gestionpresupuesto.repository
 
 import android.util.Log
 import com.example.gestionpresupuesto.entities.Product
+import com.example.gestionpresupuesto.fragments.menu.containerFragmentProduct.ProductDetail
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
